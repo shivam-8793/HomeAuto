b8b40f9e043d7dcabb70ade990ed1663a5393546
Shivam Dhote <shivamdhote3@gmail.com>
2021-01-27 00:09:10 +0530

    changes as per new design (DC switches only)

--- esp_smartswitch/monitor/include/monitor.h
+++ esp_smartswitch/monitor/include/monitor.h
@@ -1,7 +1,4 @@
 #include "state_table.h"
 
-#define MAX_NORMAL_SWITCHES 3
-bool Is_Any_Switch_Toggle( device_state_table_t *, device_state_table_t * );
-bool Is_ReglSwitch_Toggle(void);
-void Get_HardSwitch_Status(void);
 void SwitchStates_Read( device_state_table_t * );
+bool Monitor_Switches( device_state_table_t *, device_state_table_t * );
--- esp_smartswitch/monitor/monitor.c
+++ esp_smartswitch/monitor/monitor.c
@@ -7,15 +7,11 @@
 #include <stdbool.h>
 #include "monitor.h"
 #include "port.h"
-#include "driver/timer.h"
 #include "state_table.h"
+#include "control.h"
 
-/* timer configuration parameters */
-#define TIMER_DIVIDER       40000uL  /* Hardware timer clock divider [ freq = 2 kHz | 1 tick = 0.5 ms ]  */
-#define TIMER_SCALE         2uL      /* convert counter value to milliseconds */
-#define TIMER_INTERVAL_MS   15uL     /* timer interval */
 
-/* internal objects definitions */
+/* =================== Local objects definitions ====================== */
 
 /* switch type definition */
 typedef enum SwitchTypeTag{
@@ -23,25 +19,9 @@ typedef enum SwitchTypeTag{
    SWITCH_TYPE_REGL    /* regulator type switch */
 }switch_type_t;
 
-static bool timeout = false;
-static bool timer_init_done = false;
 
-int zero_cross = 0;  /* @TODO : zero crossing component to be implemented */
 
-/* internal function definitions */
-
-/**
- * @brief Timer interrupt callback handler impl
- * 
- * @param void
- * @return
- *     - interrupt to be YIELD or not
- */
-bool IRAM_ATTR timer0_group0_isr_handler(void *arg)
-{
-   timeout = true;
-   return true;
-}
+/* ================= Local function definitions ======================== */
 
 /**
  * @brief Reads switch status as per switch type
@@ -54,82 +34,61 @@ bool IRAM_ATTR timer0_group0_isr_handler(void *arg)
  */
 static uint8_t read_switch_status(gpio_num_t switch_pin, switch_type_t switch_type)
 {
-   if(!timer_init_done) /* timer must be initialized */
-   { /* @TODO : create separate API for timer initialization */
-
-      /* initialize basic parameters of the timer */
-      timer_config_t config = { 
-         .divider = TIMER_DIVIDER,
-         .counter_dir = TIMER_COUNT_UP,
-         .counter_en = TIMER_PAUSE,
-         .alarm_en = TIMER_ALARM_DIS,
-         .auto_reload = TIMER_AUTORELOAD_EN,
-      }; /* timer is configured to tick at 2kz freq */
-
-      timer_init(TIMER_GROUP_0, TIMER_0, &config); /* initialize the group0 timer0 hw */
-
-      timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0uL); /* reset counter */
-
-      /* Configure the alarm for timeout value TIMER_INTERVAL_MS  */
-      timer_set_alarm_value(TIMER_GROUP_0, TIMER_0, TIMER_INTERVAL_MS * TIMER_SCALE);
-      timer_enable_intr(TIMER_GROUP_0, TIMER_0);
-
-      /* register isr callback for group0 timer0 */
-      timer_isr_callback_add(TIMER_GROUP_0, TIMER_0, timer0_group0_isr_handler,NULL, ESP_INTR_FLAG_IRAM); 
-   }
-
    uint8_t ret_value = STATE_OFF;
-
-   timer_set_alarm(TIMER_GROUP_0,TIMER_0,TIMER_ALARM_EN); /* enable alarm */
 
    if( SWITCH_TYPE_REGL == switch_type )
    {
-      while(zero_cross); /* wait for zero cross before timer starts so that pulse can be read accurately */
-      timer_start(TIMER_GROUP_0, TIMER_0);
+      /* read pot value/ read from regl unit */
    }
    else
    {
-      timer_start(TIMER_GROUP_0, TIMER_0);
+      ret_value = gpio_get_level(switch_pin);
    }
 
-   while(timeout)
+return ret_value;
+}
+
+
+bool Monitor_Switches( device_state_table_t *device_states_buff_ptr, device_state_table_t *switch_states_buff_ptr )
+{  /* test code to read switch_1 pulse continuously */
+   //printf("%d\n",gpio_get_level(SWITCH_1));
+   /*end of test code */
+   
+   device_state_table_t current_switch_states; /* temporary buffer to read current switch statuses */
+   SwitchStates_Read(&current_switch_states);
+
+   if( Is_Automation_on() )
    {
-      if(gpio_get_level(switch_pin)) /* pulse change before timeout */
+      if( current_switch_states.dev_1 != switch_states_buff_ptr->dev_1 )
       {
-         if( SWITCH_TYPE_REGL == switch_type )
-         {
-            timer_pause(TIMER_GROUP_0, TIMER_0);
-            timer_get_counter_value(TIMER_GROUP_0, TIMER_0, (uint64_t *)&ret_value);
-            ret_value = 10 - (ret_value/TIMER_SCALE); /* calculate speed of regulator switch */
-         }
-         else
-         {
-            ret_value = STATE_ON;
-         }
-
-         break;
+         device_states_buff_ptr->dev_1 = ~(device_states_buff_ptr->dev_1);
       }
+
+      if( current_switch_states.dev_2 != switch_states_buff_ptr->dev_2 )
+      {
+         device_states_buff_ptr->dev_2 = ~(device_states_buff_ptr->dev_2);
+      }
+
+      if( current_switch_states.dev_3 != switch_states_buff_ptr->dev_3 )
+      {
+         device_states_buff_ptr->dev_3 = ~(device_states_buff_ptr->dev_3);
+      }
+
+      if( current_switch_states.dev_reg1 != switch_states_buff_ptr->dev_reg1 )
+      {
+      }
+
+   }
+   else
+   {
+      *device_states_buff_ptr = current_switch_states;
    }
 
-   /* if no pulse change within timeout value, switch is off */
+   *switch_states_buff_ptr = current_switch_states;
 
-   /* disable and reset alarm */
-   timer_set_alarm(TIMER_GROUP_0,TIMER_0,TIMER_ALARM_DIS);
-   timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0uL);
-
-   return ret_value;
-}
-
-bool Is_Any_Switch_Toggle( device_state_table_t *device_states_buff_ptr, device_state_table_t *switch_states_buff_ptr )
-{
    return false;
 }
 
-
-bool Is_ReglSwitch_Toggle(void)
-{
-   return false;
-}
 
 
 /**
@@ -157,4 +116,5 @@ void SwitchStates_Read( device_state_table_t *switch_states_buff_ptr )
 /*================================================== Revision History =====================================================*\
    Author         Rev Date       Comments
  * Shivam         12-12-2020     Initial Version. switch state read implementation.
+ * Shivam         02-01-2021      Changes as per new design (DC switch Design).
 \*=========================================================================================================================*/
